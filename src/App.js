82d3e14cd192945b90bc0b1103ed6e25ef997bc2
yunamoon <tpwkao0051@gmail.com>
2024-03-07 23:44:45 +0900

    css update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const { Component } = require("react");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <main className="main-container">
+                <h1 >가계부</h1>
+                <div style={{width :'100%', backgroundColor:'white', padding:'1rem'}}>
+                <p>입력</p>
+                </div>
+
+                <div style={{display : 'flex' , justifyContent:'end' , marginTop : '1rem'}}>
+                <p style={{fontSize:'2rem'}}>
+                    총합:
+                    <span>원</span>
+                </p>
+                </div>  
+            </main>
+        )
+    }
 }
 
 export default App;
